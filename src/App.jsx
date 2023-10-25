3c439f2425e33af62569d476f30aaae04f88264b
MartinVillegas25 <martinvillegas90@hotmail.com>
2023-10-25 08:32:32 -0300

    componentes

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,8 @@ import ClientDashboardMenu from './Components/ClientDashboard/ClientDashboardMen
 import ClientDashboardConfig from './Components/ClientDashboard/ClientDashboardConfig';
 import Menu from './Components/Menu/Menu';
 import QrGenerator from './Components/ClientDashboard/ClientConfig/QrGenerator';
-// import Gracias from './Components/MainPage/Gracias/gracias';
-import LoginAdmin from './Components/MainPage/LoginAdmin/loginAdmin';
+import Gracias from './Components/MainPage/Gracias/Gracias';
+import LoginAdmin from './Components/MainPage/LoginAdmin/LoginAdmin';
 import AlertChart from './Components/Menu/AlertChart/AlertChart';
 import ClientChat from './Components/ClientDashboard/ClientChat/ClientChat';
 function App() {
@@ -21,7 +21,7 @@ function App() {
 					{/* Ruta de pagina principal  */}
 					<Route path="/" element={<MainPage />} />
 					<Route path="/home" element={<MainPage />} />
-					{/* <Route path="/gracias" element={<Gracias />} /> */}
+					<Route path="/gracias" element={<Gracias />} />
 					<Route path="/admin-boss" element={<LoginAdmin />} />
 					{/* Rutas del panel de administrador */}
 					<Route path="/admin" element={<AdminDashboardHome />} />
